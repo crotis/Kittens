2539f5709d41d3fc590044ea1869fd9eaf2ad629
Chris Rotis <zeuri@chriss-mbp.home>
2017-03-22 13:40:12 +0000

    Updated Info component, now matches the README

--- src/js/pages/Info.js
+++ src/js/pages/Info.js
@@ -7,78 +7,78 @@ export default class Info extends React.Component {
     return (
       <div>
       <br />
+
         <h3>My Message:</h3>
-        <p> hey guys! Ive had a great time developing this application, although since i have very little
-	          experianced with React, JS and even with web-development in general it has meant for quite a steep
-	          learning curve in such a short period!
+        <p>
+            Hey guys! I've had a great time developing the application I now lovingly call "Kitty-Paste!".
+	          There was a slight learning curve at first with React but I've come to really enjoy using the Library
+	          so I hope that my work is to your satisfaction!
         </p>
-        <h3>View Application:</h3>
-        <p> I dont currently have a server I can use so we will be running the Application in localhost.
-	          Clone the repository to your desired directory.
-	          Go to kittens/src then open Index.html
-        </p>
+
         <h3>The Application:</h3>
         <p>
-        As of midday Tuesday (21/03/2017) it will have been 48 hours since I started developing the application,
-        so I wanted to give you an idea of where I'm currently at.
-        I do however plan on continuing with development and updating the project repository untill completion!
-        So I hope the work so far is to your satisfaction!
+          The components are structured into a single paged application in 'index.js' using
+  	      React libraries (Router, Route, IndexRoute & hashHistory).
         </p>
-        <p>
-          I currently have the application working as a SPA (single-page-application). Each component
-	       (Kittens, Paste and Info) are imported into the 'Layout' component which handles the overall layout of
-	        the application.
-        </p>
-        <p>
-          The 'Kittens' tab when loaded for the first time calls the Devtest API and retrieves the URL's.
-	        This data is then saved into the components state, mapped and rendered.
-	        When one of the Kitten images is clicked, the 'Paste' component is fired and will recieve the relevent
-	        image data. Currently the Paste component opens but the data is not recieved, Im currently working with
-	        'React Flux' to make the data availible to the component and its not far from working!
-       </p>
-       <p>
-          The 'Paste' component will allow the user to input their credentials as well as to specify options such
-	        as (public/private, duration). The user can then create a new pastebin post from this data! If no data,
-	        or no user credentials are specified, the app will use defauls and/or an anonymous post.
-	        Unfortunately currently the post aren't working, I ran into some issue with an
-          'Access-Control-Allow-Origin' error, which means you can't access the API from certain origins,
-	        (such as a localhost) which is what I've been using for development! I've gotten past the error but am
-	        still having issues with bad responses. I have gotten good responses and created pastes via API mapping
-	        using Postman but I have yet to do so in the application.
-      </p>
-      <p>
-          The 'Info' component just provides a fun page to read this message, which is also in the 'README.md'
-	        file in the GitHub Repository.
-      </p>
-      <h3>Further Development:</h3>
-      <p>
-          I am disappointed I did not get further in the 48 hours, however I plan on continuing the
-		      applications development and completing the following:
-      </p>
-      <li>FLUX data storage</li>
-      <li>PasteBin Posts and displaying the response data such as the PasteBin link</li>
-      <li>Unit and Itegration Testing</li>
 
-      <h3>Developer Concers:</h3>
-      <h4>
-        Requirments (These should all be installed via npm in the projects working directory):
-      </h4>
-      <li>React</li>
-      <li>react-router (For directing between components)</li>
-      <li>webpack - Bundler for modules, also allows live updating of application via command
-				  "webpack --watch"</li>
-      <h4>Scripts:</h4>
+       <p>
+          The 'Layout' component simply handles the overall layout of all other components:
+          (Kitten, Info, Header and Footer).
+      </p>
+
       <p>
-        I added "dev: {"webpack-dev-server --content-base src --inline --hot"} to package.json
-		    This will allow for simpler compilation: simply run the command "npm run dev"
-      </p>
-      <h4>Development:</h4>
-      <p>When developing, run in seprate terminals the following commands (Their uses are described above):
-      </p>
-      <p>When developing, run the following commans in seperate terminals</p>
-      <li>npm run dev </li>
-      <li>watchman --watch</li>
-      </div>
-    );
-  }
+      The Kitten component is where the real fun happens! When this component mounts it automatically
+      sends a GET request to DevTests API and recieves the image paths in the response. The JSON array is
+      iterated through and img tages are created for each path and injected into the HTML.
+     </p>
+
+     <p>
+        The user is then able to click any of these images. Upon doing so a POST request is sent to the
+        DevTest API and the response is made availible in the textarea's. This also required converting an
+        array of characters to a string (which also got rid of all the commers in the response). I refactored
+        this method into Util/Tools and imported it into the class.
+    </p>
+        The 'Info' component just provides a fun page to read this information, which is also in the
+        'README.md'file in the GitHub Repository.
+    <p>
+
+    </p>
+
+        <h3>Further Development:</h3>
+        <p>
+          I'm in the process of removing the "undefined" text which appears in the textbox before the POST response.
+          I think the problem occurs when the components state updates after the second API call.
+       </p>
+
+       <p>
+          I usually add unit and integration tests to my code as I go (TDD style). However since I was using a new
+	        framework I thought this might be abit much to begin with. Though in retrospect I think it would of helped
+	        me rather than hindered me! Unfortunately I haven't yet managed to add the tests. I have looked into testing
+	        using Enzym, Expect and Mocha and will be adding them over the next day or so as well as cleaning up the code
+	        and improving on the UI.
+       <p>
+
+     <h3>Developer Concers:</h3>
+     <h4>
+       Requirments (These should all be installed via npm in the projects working directory):
+     </h4>
+     <li>React</li>
+     <li>react-router (For directing between components)</li>
+     <li>webpack - Bundler for modules, also allows live updating of application via command
+         "webpack --watch"</li>
+
+     <h4>Scripts:</h4>
+     <p>
+       I added "dev: {"webpack-dev-server --content-base src --inline --hot"} to package.json
+       This will allow for simpler compilation: simply run the command "npm run dev"
+     </p>
+     <h4>Development:</h4>
+     <p>When developing, run in seprate terminals the following commands (Their uses are described above):
+     </p>
+     <p>When developing, run the following commans in seperate terminals</p>
+     <li>npm run dev </li>
+     <li>watchman --watch</li>
+     </div>
+   );
+ }
 }
