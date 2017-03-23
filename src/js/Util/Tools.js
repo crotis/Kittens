ffabd83633fe2f82e053b87320d1e309653f2ef2
Chris Rotis <zeuri@chriss-mbp.home>
2017-03-23 10:36:49 +0000

    BugFix: Undefined in Post Response removed

--- src/js/Util/Tools.js
+++ src/js/Util/Tools.js
@@ -1,12 +1,11 @@
 var arrayToString = function(arr)
 {
-  var str;
+  var str = "";
   var i;
 
   for (i=0; i<arr.length; i++){
     str += arr[i];
   }
-  // str.replace("undefined", "");
   return str;
 }
 
